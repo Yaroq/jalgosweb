80ecf09d828eafb914dcb880c77364a9fb805008
yaroq <julien.muresianu@gmail.com>
2015-05-04 19:00:14 +0200

    usecases

--- src/jalgosweb/routes/home.clj
+++ src/jalgosweb/routes/home.clj
@@ -7,9 +7,44 @@
   (layout/render
     "home.html"))
 
+(defn about-page []
+  (layout/render
+    "about.html"))
+
+(defn usecases-page []
+  (layout/render
+    "usecases.html"))
+
+(defn contact-page []
+  (layout/render
+    "contact.html"))
+
+
+
 (defn fr-page []
-  (layout/render "fr/home.html"))
+  (layout/render "home_fr.html"))
+
+(defn fr-about-page []
+  (layout/render
+    "about_fr.html"))
+
+(defn fr-usecases-page []
+  (layout/render
+    "usecases_fr.html"))
+
+(defn fr-contact-page []
+  (layout/render
+    "contact_fr.html"))
+
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/fr" [] (fr-page)))
+  (GET "/about" [] (about-page))
+  (GET "/usecases" [] (usecases-page))  
+  (GET "/contact" [] (contact-page))
+
+  (GET "/fr" [] (fr-page))
+  (GET "/about-fr" [] (fr-about-page))
+  (GET "/usecases-fr" [] (fr-usecases-page))  
+  (GET "/contact-fr" [] (fr-contact-page))
+)
