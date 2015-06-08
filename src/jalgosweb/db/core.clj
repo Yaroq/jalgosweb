f36e0463cc63b8f99577c4a67ca22944c42aa821
yaroq <julien.muresianu@gmail.com>
2015-06-08 14:48:47 +0200

    fixed commits that were on bad branch again

--- src/jalgosweb/db/core.clj
+++ src/jalgosweb/db/core.clj
@@ -3,3 +3,20 @@
               [monger.collection :as mc]
               [monger.operators :refer :all]
               [monger.conversion :refer [from-db-object]]))
+
+
+(defonce db (let [uri (get (System/getenv) "MONGOHQ_URL"
+                           "mongodb://127.0.0.1/jalgosweb")
+                  {:keys [conn db]} (mg/connect-via-uri uri)]
+              db))
+
+(defn create-user [user]
+  (mc/insert db "users" user))
+
+(defn update-user [id first-name last-name email]
+  (mc/update db "users" {:id id}
+             {$set {:first_name first-name
+                    :last_name last-name
+                    :email email}}))
+(defn get-user [id]
+  (mc/find-one-as-map db "users" {:id id}))
